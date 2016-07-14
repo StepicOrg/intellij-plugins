fcc03d6fb62f583326eaa1420cf97a0a69b289d3
2Pit <afnm47@gmail.com>
2016-07-14 20:46:58 +0300

    first commit

---
+++ stepic-java/src/org/stepic/plugin/java/actions/StepicJavaPostAction.java
@@ -0,0 +1,22 @@
+package org.stepic.plugin.java.actioons;
+
+import com.intellij.openapi.project.Project;
+import com.jetbrains.edu.learning.actions.StudyCheckAction;
+import org.jetbrains.annotations.NotNull;
+
+public class StepicJavaPostAction extends StudyCheckAction {
+
+    @Override
+    public void check(@NotNull Project project) {
+
+    }
+
+
+    @NotNull
+    @Override
+    public String getActionId() {
+        return "StepicJavaPostAction";
+    }
+
+
+}
