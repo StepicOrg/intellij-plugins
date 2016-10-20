e5f1412eb2ac08b17c8b48656721f9d826fd19e8
meanmail <meanmail@mail.ru>
2016-10-20 21:27:26 +0700

    Added gradle scripts. Fix path to resources in classes

--- core/src/main/java/icons/InteractiveLearningIcons.java
+++ core/src/main/java/icons/InteractiveLearningIcons.java
@@ -13,19 +13,19 @@ public class InteractiveLearningIcons {
     return IconLoader.getIcon(path, InteractiveLearningIcons.class);
   }
 
-  public static final Icon CheckTask = load("/core/icons/com/jetbrains/edu/learning/CheckTask.png"); // 16x16
-  public static final Icon Course = load("/core/icons/com/jetbrains/edu/learning/Course.png"); // 16x16
-  public static final Icon CourseProgress = load("/core/icons/com/jetbrains/edu/learning/CourseProgress.png"); // 13x13
-  public static final Icon InterpreterGear = load("/core/icons/com/jetbrains/edu/learning/interpreterGear.png"); // 16x16
-  public static final Icon Lesson = load("/core/icons/com/jetbrains/edu/learning/Lesson.png"); // 16x16
-  public static final Icon LessonCompl = load("/core/icons/com/jetbrains/edu/learning/LessonCompl.png"); // 16x16
-  public static final Icon Prev = load("/core/icons/com/jetbrains/edu/learning/prev.png"); // 16x16
-  public static final Icon ResetTaskFile = load("/core/icons/com/jetbrains/edu/learning/ResetTaskFile.png"); // 16x16
-  public static final Icon Sandbox = load("/core/icons/com/jetbrains/edu/learning/Sandbox.png"); // 16x16
-  public static final Icon ShowHint = load("/core/icons/com/jetbrains/edu/learning/showHint.png"); // 16x16
-  public static final Icon Task = load("/core/icons/com/jetbrains/edu/learning/Task.png"); // 16x16
-  public static final Icon TaskCompl = load("/core/icons/com/jetbrains/edu/learning/TaskCompl.png"); // 16x16
-  public static final Icon TaskDescription = load("/core/icons/com/jetbrains/edu/learning/TaskDescription.png"); // 13x13
-  public static final Icon TaskProbl = load("/core/icons/com/jetbrains/edu/learning/TaskProbl.png"); // 16x16
-  public static final Icon WatchInput = load("/core/icons/com/jetbrains/edu/learning/WatchInput.png"); // 24x24
+  public static final Icon CheckTask = load("/icons/com/jetbrains/edu/learning/CheckTask.png"); // 16x16
+  public static final Icon Course = load("/icons/com/jetbrains/edu/learning/Course.png"); // 16x16
+  public static final Icon CourseProgress = load("/icons/com/jetbrains/edu/learning/CourseProgress.png"); // 13x13
+  public static final Icon InterpreterGear = load("/icons/com/jetbrains/edu/learning/interpreterGear.png"); // 16x16
+  public static final Icon Lesson = load("/icons/com/jetbrains/edu/learning/Lesson.png"); // 16x16
+  public static final Icon LessonCompl = load("/icons/com/jetbrains/edu/learning/LessonCompl.png"); // 16x16
+  public static final Icon Prev = load("/icons/com/jetbrains/edu/learning/prev.png"); // 16x16
+  public static final Icon ResetTaskFile = load("/icons/com/jetbrains/edu/learning/ResetTaskFile.png"); // 16x16
+  public static final Icon Sandbox = load("/icons/com/jetbrains/edu/learning/Sandbox.png"); // 16x16
+  public static final Icon ShowHint = load("/icons/com/jetbrains/edu/learning/showHint.png"); // 16x16
+  public static final Icon Task = load("/icons/com/jetbrains/edu/learning/Task.png"); // 16x16
+  public static final Icon TaskCompl = load("/icons/com/jetbrains/edu/learning/TaskCompl.png"); // 16x16
+  public static final Icon TaskDescription = load("/icons/com/jetbrains/edu/learning/TaskDescription.png"); // 13x13
+  public static final Icon TaskProbl = load("/icons/com/jetbrains/edu/learning/TaskProbl.png"); // 16x16
+  public static final Icon WatchInput = load("/icons/com/jetbrains/edu/learning/WatchInput.png"); // 24x24
 }
