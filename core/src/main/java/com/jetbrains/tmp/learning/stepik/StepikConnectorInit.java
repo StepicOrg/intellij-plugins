58b6414dacc90a4a950aebbb1bab5b3c51250658
2Pit <afnm47@gmail.com>
2016-10-27 17:53:30 +0300

    rf

--- core/src/main/java/com/jetbrains/tmp/learning/stepik/StepikConnectorInit.java
+++ core/src/main/java/com/jetbrains/tmp/learning/stepik/StepikConnectorInit.java
@@ -1,19 +1,3 @@
-
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.jetbrains.tmp.learning.stepik;
 
 import com.intellij.openapi.diagnostic.Logger;
@@ -51,7 +35,6 @@ public class StepikConnectorInit {
 
     public static HttpClientBuilder getBuilder() {
         HttpConfigurable instance = HttpConfigurable.getInstance();
-
         try {
             TrustManager[] trustAllCerts = getTrustAllCerts();
             SSLContext sslContext = SSLContext.getInstance("TLS");
@@ -64,7 +47,7 @@ public class StepikConnectorInit {
             if (instance.USE_HTTP_PROXY) {
                 HttpHost host = new HttpHost(instance.PROXY_HOST, instance.PROXY_PORT);
                 httpClientBuilder.setProxy(host);
-                logger.info("Uses proxy. Host = " + instance.PROXY_HOST + "; Port = " + instance.PROXY_PORT);
+                logger.info("Uses proxy: Host = " + instance.PROXY_HOST + " Port = " + instance.PROXY_PORT);
             }
             return httpClientBuilder;
         } catch (NoSuchAlgorithmException | KeyManagementException e) {
