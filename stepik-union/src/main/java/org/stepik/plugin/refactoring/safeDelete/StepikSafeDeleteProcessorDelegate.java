f7144841d09dabaebe175bf1d560ac1b6a10f0a1
meanmail <meanmail@mail.ru>
2016-11-14 21:26:59 +0700

    Replaced the error message when user run "Safe delete"

--- stepik-union/src/main/java/org/stepik/plugin/refactoring/safeDelete/StepikSafeDeleteProcessorDelegate.java
+++ stepik-union/src/main/java/org/stepik/plugin/refactoring/safeDelete/StepikSafeDeleteProcessorDelegate.java
@@ -66,7 +66,7 @@ public class StepikSafeDeleteProcessorDelegate extends SafeDeleteProcessorDelega
 
     @Override
     public void prepareForDeletion(PsiElement element) throws IncorrectOperationException {
-        throw new IncorrectOperationException("Don't delete this file");
+        throw new IncorrectOperationException("The operation is suspended. Course structure might become defective");
     }
 
     @Override
