c7bdd151b8ebb7066bd180379b632d4f0f040726
2Pit <afnm47@gmail.com>
2016-11-14 19:29:51 +0300

    refactoring MetricUtils

--- core/src/main/java/com/jetbrains/tmp/learning/stepik/metric/MetricUtils.java
+++ core/src/main/java/com/jetbrains/tmp/learning/stepik/metric/MetricUtils.java
@@ -1,15 +1,15 @@
 package com.jetbrains.tmp.learning.stepik.metric;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Arrays;
 
 public class MetricUtils {
-    public static boolean isAllNull(Object... objects) {
-        if (objects == null) return true;
+    public static boolean isAllNull(@NotNull Object... objects) {
         return Arrays.stream(objects).allMatch((x) -> x == null);
     }
 
-    public static boolean isAnyNull(Object... objects) {
-        if (objects == null) return true;
+    public static boolean isAnyNull(@NotNull Object... objects) {
         return Arrays.stream(objects).anyMatch((x) -> x == null);
     }
 }
