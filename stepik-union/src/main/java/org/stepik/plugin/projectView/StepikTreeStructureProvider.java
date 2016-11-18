00bf8b18c54efc9ef34434dc7a1f143331f3b59c
meanmail <meanmail@mail.ru>
2016-11-19 03:38:17 +0700

    Showing subclasses

--- stepik-union/src/main/java/org/stepik/plugin/projectView/StepikTreeStructureProvider.java
+++ stepik-union/src/main/java/org/stepik/plugin/projectView/StepikTreeStructureProvider.java
@@ -2,12 +2,13 @@ package org.stepik.plugin.projectView;
 
 import com.intellij.ide.projectView.TreeStructureProvider;
 import com.intellij.ide.projectView.ViewSettings;
-import com.intellij.ide.projectView.impl.nodes.PsiFileNode;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiFile;
 import com.jetbrains.tmp.learning.StudyTaskManager;
 import com.jetbrains.tmp.learning.core.EduNames;
 import com.jetbrains.tmp.learning.courseFormat.Course;
@@ -36,15 +37,18 @@ public class StepikTreeStructureProvider implements TreeStructureProvider, DumbA
         for (AbstractTreeNode node : children) {
             final Project project = node.getProject();
             if (project != null) {
-                if (node.getValue() instanceof PsiDirectory) {
-                    final PsiDirectory nodeValue = (PsiDirectory) node.getValue();
+                Object value = node.getValue();
+                if (value instanceof PsiDirectory) {
+                    final PsiDirectory nodeValue = (PsiDirectory) value;
                     if (isVisibleDirectory(nodeValue)) {
                         nodes.add(new StepikDirectoryNode(project, nodeValue, settings));
                     }
-                } else if (node instanceof PsiFileNode) {
-                    if (isVisibleFile(((PsiFileNode) node).getValue())) {
+                } else if (value instanceof PsiFile) {
+                    if (isVisibleFile((PsiFile) value)) {
                         nodes.add(node);
                     }
+                } else if (value instanceof PsiClass) {
+                    nodes.add(node);
                 }
             }
         }
