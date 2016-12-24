7a4cba797df8981cc6cf9bc2b1284672d224cee5
2Pit <afnm47@gmail.com>
2016-12-24 15:49:52 +0300

    fix names

--- core/src/main/java/icons/InteractiveLearningIcons.java
+++ core/src/main/java/icons/InteractiveLearningIcons.java
@@ -11,17 +11,17 @@ public class InteractiveLearningIcons {
     }
 
     public static final Icon Course = load("/icons/com/jetbrains/edu/learning/course.png"); // 16x16
-    public static final Icon CourseCorrect = load("/icons/com/jetbrains/edu/learning/course_correct.png"); // 16x16
-    public static final Icon CourseWrong = load("/icons/com/jetbrains/edu/learning/course_wrong.png"); // 16x16
+    public static final Icon CourseCorrect = load("/icons/com/jetbrains/edu/learning/courseCorrect.png"); // 16x16
+    public static final Icon CourseWrong = load("/icons/com/jetbrains/edu/learning/courseWrong.png"); // 16x16
     public static final Icon Module = load("/icons/com/jetbrains/edu/learning/module.png"); // 16x16
-    public static final Icon ModuleCorrect = load("/icons/com/jetbrains/edu/learning/module_correct.png"); // 16x16
-    public static final Icon ModuleWrong = load("/icons/com/jetbrains/edu/learning/module_wrong.png"); // 16x16
+    public static final Icon ModuleCorrect = load("/icons/com/jetbrains/edu/learning/moduleCorrect.png"); // 16x16
+    public static final Icon ModuleWrong = load("/icons/com/jetbrains/edu/learning/moduleWrong.png"); // 16x16
     public static final Icon Lesson = load("/icons/com/jetbrains/edu/learning/lesson.png"); // 16x16
-    public static final Icon LessonCorrect = load("/icons/com/jetbrains/edu/learning/lesson_correct.png"); // 16x16
-    public static final Icon LessonWrong = load("/icons/com/jetbrains/edu/learning/lesson_wrong.png"); // 16x16
+    public static final Icon LessonCorrect = load("/icons/com/jetbrains/edu/learning/lessonCorrect.png"); // 16x16
+    public static final Icon LessonWrong = load("/icons/com/jetbrains/edu/learning/lessonWrong.png"); // 16x16
     public static final Icon Step = load("/icons/com/jetbrains/edu/learning/step.png"); // 16x16
-    public static final Icon StepCorrect = load("/icons/com/jetbrains/edu/learning/step_correct.png"); // 16x16
-    public static final Icon StepWrong = load("/icons/com/jetbrains/edu/learning/step_wrong.png"); // 16x16
+    public static final Icon StepCorrect = load("/icons/com/jetbrains/edu/learning/stepCorrect.png"); // 16x16
+    public static final Icon StepWrong = load("/icons/com/jetbrains/edu/learning/stepWrong.png"); // 16x16
 
     public static final Icon CheckTask = load("/icons/com/jetbrains/edu/learning/CheckTask.png"); // 16x16
     public static final Icon ResetTaskFile = load("/icons/com/jetbrains/edu/learning/ResetTaskFile.png"); // 16x16
