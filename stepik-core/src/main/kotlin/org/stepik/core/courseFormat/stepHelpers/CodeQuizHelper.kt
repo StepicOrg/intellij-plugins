817c0b24a32a0184664ba440eab0dfc38ebb38b3
meanmail <meanmail@mail.ru>
2018-02-26 12:34:59 +0700

    Indexing of samples from a unit. Hide index if there is only one sample

--- stepik-core/src/main/kotlin/org/stepik/core/courseFormat/stepHelpers/CodeQuizHelper.kt
+++ stepik-core/src/main/kotlin/org/stepik/core/courseFormat/stepHelpers/CodeQuizHelper.kt
@@ -14,9 +14,11 @@ class CodeQuizHelper(project: Project, stepNode: StepNode) : StepHelper(project,
 
     val samples: String
         get() {
-            return stepNode.samples.mapIndexed { index, sample ->
-                "<p><b>Sample Input $index:</b><br>${sample.input}<br>" +
-                        "<b>Sample Output $index:</b><br>${sample.output}"
+            val showIndex = stepNode.samples.size > 1
+            return stepNode.samples.mapIndexed { i, sample ->
+                val index = if (showIndex) " ${i + 1}" else ""
+                return@mapIndexed "<p><b>Sample Input$index:</b><br>${sample.input}<br>" +
+                        "<b>Sample Output$index:</b><br>${sample.output}"
             }.joinToString("<br>").replace("\\n".toRegex(), "<br>")
         }
 }
